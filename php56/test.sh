9b55d2c237cd064b9c85061995662b445ab5c84a
Andy Whalen <awhalen@wpi.edu>
2018-02-09 10:23:09 -0500

    php56 version

---
+++ php56/test.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+php -i
+php --version
+composer --version
+echo "Node:"
+node --version
+echo "NPM:"
+npm --version
+echo "Yarn:"
+yarn --version
+drush --version
+wp --version --allow-root
+git --version
+fab --version
+mkdocs --version
